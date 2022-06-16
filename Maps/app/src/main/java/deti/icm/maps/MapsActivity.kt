b271df914a95e3d54ca65a95b92c9223abf98a01
Isadora <flisadora@gmail.com>
2022-06-16 10:55:38 +0100

    google maps initial implementation

---
+++ Maps/app/src/main/java/deti/icm/maps/MapsActivity.kt
@@ -0,0 +1,65 @@
+package deti.icm.maps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import deti.icm.maps.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //hideStatusBar()
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //hideStatusBar()
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun hideStatusBar() {
+        // Hide the status bar.
+        Log.i("ISADORA", "hey sister")
+        WindowInsetsControllerCompat(window, window.decorView)
+            .hide(WindowInsetsCompat.Type.systemBars())
+    }
+}
--- project/app/src/main/java/deti/icm/trotinet/ui/activity/UserDetails.kt
+++ project/app/src/main/java/deti/icm/trotinet/ui/activity/UserDetails.kt
@@ -19,6 +19,11 @@ import retrofit2.Response
 
 class UserDetails : AppCompatActivity(R.layout.activity_user_details) {
 
+    private val repository by lazy {
+        AppDatabase.instance(this).appDao()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_details)
@@ -28,10 +33,14 @@ class UserDetails : AppCompatActivity(R.layout.activity_user_details) {
 
     override fun onResume() {
         super.onResume()
-        val db = AppDatabase.instance(this)
-        val appDao = db.appDao()
-        val user = appDao.getAllUsers()[0]
-        val userRides = appDao.loadUserRides(user.uid)
+
+        showUserDetails()
+        getLocation()
+    }
+
+    private fun showUserDetails() {
+        val user = repository.getAllUsers()[0]
+        val userRides = repository.loadUserRides(user.uid)
         val distance = userRides.sumOf { it.distance }
 
         val text_view_balance = findViewById<TextView>(R.id.user_details_balance)
@@ -42,26 +51,29 @@ class UserDetails : AppCompatActivity(R.layout.activity_user_details) {
         val button_save = findViewById<Button>(R.id.user_details_save)
 
         text_view_balance.text = user.balance.toString() + "€"
-        text_view_distance.text = (distance/1000).toString() +" km"
+        text_view_distance.text = (distance / 1000).toString() + " km"
         text_view_rides.text = userRides.size.toString() + " rides"
         text_input_name.text = user.name
         text_input_email.text = user.email
 
-        button_save.setOnClickListener{
+        button_save.setOnClickListener {
             val editedUser = User(
                 user.uid,
                 text_input_name.text.toString(),
                 text_input_email.text.toString(),
                 user.balance,
             )
-            appDao.updateUser(editedUser)
+            repository.updateUser(editedUser)
+            Toast.makeText(
+                applicationContext,
+                "Your new information was saved!",
+                Toast.LENGTH_LONG
+            ).show()
         }
-
-        getLocation()
     }
 
     fun getLocation() {
-       var city: String = ""
+       var city: String
        RetrofitInitializer().geolocationService.getGeodata("-19.9191", "-43.9387").enqueue(
            object : Callback<Geolocation> {
                override fun onFailure(call: Call<Geolocation>, t: Throwable) {
