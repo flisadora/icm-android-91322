09b72a0883eeae03739d7b4b25898bd9b3f60424
Isadora <flisadora@gmail.com>
2022-06-16 16:24:35 +0100

    map getting current location

--- project/app/src/main/java/deti/icm/trotinet/ui/activity/MapsActivity.kt
+++ project/app/src/main/java/deti/icm/trotinet/ui/activity/MapsActivity.kt
@@ -1,63 +1,114 @@
 package deti.icm.trotinet.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.Task
 import deti.icm.trotinet.R
 import deti.icm.trotinet.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private val requestCodeLocationPermission = 1
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //hideStatusBar()
+        hideStatusBar()
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        // Initialize the FusedLocationClient.
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MapsActivity)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
     }
+
 
     override fun onResume() {
         super.onResume()
-        //hideStatusBar()
+        hideStatusBar()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(
+                this@MapsActivity,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                requestCodeLocationPermission
+            )
+
+            return
+        }
+
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { last ->
+                val location = LatLng(last.latitude, last.longitude)
+                Log.i("ISADORA", "lat: ${last.latitude}, lon: ${last.longitude}")
+                mMap.addMarker(MarkerOptions().position(location))
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(location))
+                mMap.setMinZoomPreference(16.5F)
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed on getting current location",
+                    Toast.LENGTH_LONG).show()
+            }
+
     }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == requestCodeLocationPermission && grantResults.isNotEmpty()) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.i("ISADORA", "Permission Granted")
+            } else {
+                Toast.makeText(applicationContext, "Permission Denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
 
     private fun hideStatusBar() {
         // Hide the status bar.
         Log.i("ISADORA", "hey sister")
         WindowInsetsControllerCompat(window, window.decorView)
-            .hide(WindowInsetsCompat.Type.systemBars())
+            .hide(WindowInsetsCompat.Type.statusBars())
     }
 }
